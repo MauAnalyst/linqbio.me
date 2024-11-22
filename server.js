f03e0299b0697a6538c5f6637c3e110deeb50fa2
MauAnalyst <164758301+MauAnalyst@users.noreply.github.com>
2024-11-22 15:40:43 -0300

    ajust auh0

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const config = {
   secret: process.env.AUTH0_CLIENT_SECRET,
   baseURL: process.env.AUTH0_BASE_URL,
   clientID: process.env.AUTH0_CLIENT_ID,
-  issuerBaseURL: `${process.env.AUTH0_DOMAIN}`,
+  issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
   session: {
     cookie: {
       httpOnly: true,
